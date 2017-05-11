4bc7fc5f913b0a51800460d9a7f595f476cacac5
kennyrkun <keennymccormic+github@gmail.com>
2017-05-11 16:26:54 -0500

    More preparation.

--- sfbash/Console.cpp
+++ sfbash/Console.cpp
@@ -1,11 +1,13 @@
 #include "Console.hpp"
 
+#include <iostream>
+
 Console::Console()
 {
-
+	std::cout << "New console created." << std::endl;
 }
 
 Console::~Console()
 {
-
+	std::cout << "Console destroyed." << std::endl;
 }
--- sfbash/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <SFML\Graphics.hpp>
-#include <SFML\Audio.hpp>
-
-int main()
-{
-
-}
