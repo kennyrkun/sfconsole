ba869d34399a74cf37a0c08965d1d1687eb8a570
kennyrkun <keennymccormic+github@gmail.com>
2017-05-12 16:36:27 -0500

    implementation

--- sfbash/Console.cpp
+++ sfbash/Console.cpp
@@ -2,12 +2,55 @@
 
 #include <iostream>
 
-Console::Console()
+Console::Console(std::string $title)
+{
+	std::cout << "New console created." << std::endl;
+}
+
+Console::Console(std::string $title, sf::Vector2i dimensions)
 {
 	std::cout << "New console created." << std::endl;
 }
 
 Console::~Console()
 {
+	window.close();
+
 	std::cout << "Console destroyed." << std::endl;
+}
+
+void Console::create()
+{
+	window.create(sf::VideoMode(665, 320), "Terminal", sf::Style::Titlebar);
+	window.setPosition(sf::Vector2i(20, 20));
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::EventType::Closed)
+				window.close();
+
+			if (event.type == sf::Event::EventType::Resized)
+				std::cout << window.getSize().x << " : " << window.getSize().y << std::endl;
+		}
+
+		window.clear();
+
+		window.display();
+	}
+}
+
+void Console::close()
+{
+	window.close();
+}
+
+void Console::log(std::string message)
+{
+	std::cout << "logging" << std::endl;
+
+	sf::Text newline;
+	newline.setString(message);
 }
--- sfbash/Console.hpp
+++ sfbash/Console.hpp
@@ -1,11 +1,24 @@
 #ifndef CONSOLE_HPP
 #define CONSOLE_HPP
 
+#include <SFML\Graphics.hpp>
+
+#include <string>
+#include <vector>
+
 class Console
 {
 public:
-	Console();
+	Console(std::string $title);
+	Console(std::string $title, sf::Vector2i dimensions);
 	~Console();
+
+	void create();
+	void close();
+	void log(std::string message);
+
+	sf::RenderWindow window;
 };
 
 #endif /* CONSOLE_HPP */
+ 
